4fde1bf509c0a6aa41a6ef6e6426e0d6a71a5654
Administrator <Administrator@DESKTOP-1IGCCRS>
2021-02-02 17:15:03 +0800

    添加项目文件。

---
+++ MyOcelot/Startup.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using MMLib.Ocelot.Provider.AppConfiguration;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyOcelot
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOcelot()
+                            .AddAppConfiguration();
+            services.AddSwaggerForOcelot(Configuration);
+
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseRouting();
+            app.UseSwagger();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            app.UseStaticFiles();
+            app.UseSwaggerForOcelotUI(opt =>
+            {
+                opt.DownstreamSwaggerHeaders = new[]
+                {
+                        new KeyValuePair<string, string>("Key", "Value"),
+                        new KeyValuePair<string, string>("Key2", "Value2"),
+                    };
+            })
+            .UseOcelot()
+            .Wait();
+        }
+    }
+}
